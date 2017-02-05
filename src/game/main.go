38e21035b7460a0ac69840620226008a233fd23e
xinhp <go.git@qq.com>
2017-02-05 19:59:33 +0800

    test etcd file

--- src/game/main.go
+++ src/game/main.go
@@ -1,44 +1,36 @@
 package main
 
 import (
-	"github.com/coreos/etcd/client"
+	etcdclient "github.com/coreos/etcd/client"
 	"fmt"
 	"golang.org/x/net/context"
+    "strings"
 )
 
-const   DEFAULT_SERVIE_KEY   = "/redis"
+const 	DEFAULT_NAME_FILE    = "/backends/names"
 
 
 func main()  {
 	machines := []string{"http://172.17.0.2:2379"}
-	cfg := client.Config{
+	cfg := etcdclient.Config{
 		Endpoints:machines,
-		Transport:client.DefaultTransport,
+		Transport:etcdclient.DefaultTransport,
 	}
-    cli, err := client.New(cfg)
+    client, err := etcdclient.New(cfg)
     if err != nil {
         fmt.Println(err)
     }
 
-    kApi := client.NewKeysAPI(cli)
-    //rsp, err := kApi.Get(context.Background(), DEFAULT_NAME_FILE, nil)
-    //if err != nil {
-    //    fmt.Println(err)
-    //}
-    //
-    //if rsp.Node.Dir {
-    //    fmt.Println("names is not a file")
-    //}
-    //
-    //
-    //fmt.Println(rsp.Node.Value)
-    //fmt.Println(strings.Split(rsp.Node.Value, "\n"))
-
-    rsp, err := kApi.Get(context.Background(), DEFAULT_SERVIE_KEY, nil)
+    kApi := etcdclient.NewKeysAPI(client)
+    rsp, err := kApi.Get(context.Background(), DEFAULT_NAME_FILE, nil)
     if err != nil {
         fmt.Println(err)
     }
-    fmt.Println("continue")
+
+    if rsp.Node.Dir {
+        fmt.Println("names is not a file")
+    }
 
     fmt.Println(rsp.Node.Value)
+    fmt.Println(strings.Split(rsp.Node.Value, "\n"))
 }
